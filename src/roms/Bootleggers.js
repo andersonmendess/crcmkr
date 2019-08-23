2b7dbceba53f6bdbaa1bf6f4c92893dbd7da9aa4
Anderson Mendes <anderson2000ano@gmail.com>
2019-08-22 23:56:56 -0300

    add Bootleggers

---
+++ src/roms/Bootleggers.js
@@ -0,0 +1,5 @@
+export default {
+  name: 'Bootleggers ROM',
+  img: 'https://avatars1.githubusercontent.com/u/14201107?s=200&v=4',
+  primaryTextColor: '#6C11FC',
+};
--- src/roms/index.js
+++ src/roms/index.js
@@ -2,16 +2,20 @@ import LineageOS from './LineageOS';
 import PixelExperience from './PixelExperience';
 import KrakenProject from './KrakenProject';
 import HavocOS from './HavocOS';
+import Bootleggers from './Bootleggers';
+
 
 export default {
   list: [
     'LineageOS',
     'PixelExperience',
     'KrakenProject',
-    'HavocOS'
+    'HavocOS',
+    'Bootleggers'
   ],
   LineageOS,
   PixelExperience,
   KrakenProject,
-  HavocOS
+  HavocOS,
+  Bootleggers
 };
