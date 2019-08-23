250ae4afb1d6294037978b8a93503fa742baea5c
Anderson Mendes <anderson2000ano@gmail.com>
2019-08-22 23:53:03 -0300

    add havoc bcoz havoc bes

---
+++ src/roms/HavocOS.js
@@ -0,0 +1,5 @@
+export default {
+  name: 'Havoc-OS',
+  img: 'https://avatars2.githubusercontent.com/u/39052374?s=200&v=4',
+  primaryTextColor: '#2180EA',
+};
--- src/roms/index.js
+++ src/roms/index.js
@@ -1,14 +1,17 @@
 import LineageOS from './LineageOS';
 import PixelExperience from './PixelExperience';
 import KrakenProject from './KrakenProject';
+import HavocOS from './HavocOS';
 
 export default {
   list: [
     'LineageOS',
     'PixelExperience',
     'KrakenProject',
+    'HavocOS'
   ],
   LineageOS,
   PixelExperience,
   KrakenProject,
+  HavocOS
 };
